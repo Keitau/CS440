4b6a04de65b6963b603e4a221ad4b90f0945f764
Marcus McCain <mmccain@acm.org>
2013-02-04 12:52:52 -0500

    Update TicTacToe/TTT.h

--- TicTacToe/TTT.h
+++ TicTacToe/TTT.h
@@ -7,12 +7,32 @@ public:
 	TicTacToe();
 
 	void render();
-	bool tick(int* input);
+	RET_CODE tick(char* input);
 
 	int getPlayerTurn(){return playerTurn;}
 	String getCurrentPlayerName();
+	bool isWon(DIRECTION dir);
+	
+	// Code for the return of the input
+	enum RET_CODE{
+		CONTINUE,
+		PLAYER_WIN,
+		INVALID_CHOICE
+	};
+	
+	// Direction of searching board for player win
+	enum DIRECTION{
+		HORIZONTAL,
+		VERTICAL,
+		R_DIAG,
+		L_DIAG
+	};
 
 private:
+
+	//recursive function used to find if player has 3 marks in a row
+	int isWon(int currPosition, DIRECTION dir);
+	
 	int* gameBoard;
 	Player** players;
 	int playerTurn;
